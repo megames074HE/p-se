08dd88370debed995f84f8fde462063556f483dc
megames074HE <mees.vandenbos@elev.ga.ntig.se>
2025-11-04 13:38:10 +0100

    delete

--- påse.py
+++ påse.py
@@ -5,6 +5,7 @@ bag = []
 while run:
     print('Visa innehållet i påsen [V]')
     print('Spara i påsen [S]')
+    print('Ta bort nåt ur påsen [D]')
     print('Avsluta [Q]')
     choice = input('Välj: ').lower()
     if choice == 'q':
@@ -13,7 +14,20 @@ while run:
         for thing in bag:
             print(thing)
     elif choice == 's':
-        bag.append(input('skriv vad du vill spara: '))
+        
+        if len(bag) > 10:
+            print('Du har inte en sånt stor påse')
+        else:
+            bag.append(input('skriv vad du vill spara: '))
+            print(f'du har {10 - len(bag)} platser kvar')
+        
+    elif choice == 'd':
+        for thing in bag:
+            print(thing)
+        try:
+            bag.remove(input('skriv vad du vill ta bort: '))
+        except ValueError:
+            print('Naah den finns inte i listan testa igen.')
     else:
         print('Fel kommando, testa igen')
     
