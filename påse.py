3c641d18f1ea57ad06255fa656812429b8f336f0
megames074HE <mees.vandenbos@elev.ga.ntig.se>
2025-11-07 09:34:30 +0100

    färger och nya funktioner på pås appen

--- påse.py
+++ påse.py
@@ -1,33 +1,56 @@
+from colorama import Fore, Style
+
 run = True
-print('vällkommen till påsen')
+print(Fore.BLUE, 'vällkommen till påsen 💰', Style.RESET_ALL)
 bag = []
 
 while run:
-    print('Visa innehållet i påsen [V]')
+    print(Fore.YELLOW, '\nVisa innehållet i påsen  [V]')
     print('Spara i påsen [S]')
     print('Ta bort nåt ur påsen [D]')
-    print('Avsluta [Q]')
-    choice = input('Välj: ').lower()
+    print('Sök nåt i påsen och ändra/ta bort den [F]')
+    print('Avsluta [Q]', Style.RESET_ALL)
+    choice = input('\nVälj: ').lower()
     if choice == 'q':
         run = False
     elif choice == 'v':
-        for thing in bag:
-            print(thing)
+        if len(bag) == 0:
+            print(Fore.RED, '\nJodu det verkar lite tomt här. Testa adda grejer till påsen', Style.RESET_ALL)
+        else:
+            bag_sorted = sorted(bag)
+            for thing in bag_sorted:
+                print(Fore.GREEN, thing, Style.RESET_ALL)
     elif choice == 's':
         
         if len(bag) > 10:
-            print('Du har inte en sånt stor påse')
+            print(Fore.RED, '\nDu har inte en sånt stor påse', Style.RESET_ALL)
         else:
-            bag.append(input('skriv vad du vill spara: '))
-            print(f'du har {10 - len(bag)} platser kvar')
+            bag.append(input('\nskriv vad du vill spara: ').lower())
+            print(Fore.GREEN, f'\ndu har {10 - len(bag)} platser kvar', Style.RESET_ALL)
+    elif choice == 'f':
+        query = input('\nVad vill du söka efter: ')
+        if query.lower() in bag:
+            print(Fore.GREEN, f'\nHittade: "{query}" in bag', Style.RESET_ALL)
+            choice = input(f'Vill du ta bort eller ändra {query}? [D]/[C]/[NEJ] ').lower()
+            
+            if choice == 'd':
+                bag.remove(query)
+                print(Fore.GREEN, 'Den blev bortagen', Style.RESET_ALL)
+            elif choice == 'c':
+                new_query = input(f'Till vad vill du ändra {query}? ').lower()
+                bag.remove(query)
+                bag.append(new_query)
+                print(Fore.GREEN, 'Den har ändrats nu!', Style.RESET_ALL)
+
         
     elif choice == 'd':
         for thing in bag:
             print(thing)
         try:
-            bag.remove(input('skriv vad du vill ta bort: '))
+            bag.remove(input('\nskriv vad du vill ta bort: ').lower())
+            print(Fore.GREEN, 'Den blev bortagen', Style.RESET_ALL)
         except ValueError:
-            print('Naah den finns inte i listan testa igen.')
+            print(Fore.RED, '\nNaah den finns inte i listan testa igen.', Style.RESET_ALL)
     else:
-        print('Fel kommando, testa igen')
+        print(Fore.RED, '\nFel kommando, testa igen', Style.RESET_ALL)
     
